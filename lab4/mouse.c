2d6c173c42e21e21198875018c5665669cd2ce20
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-10-30 02:02:57 +0000

    Mais coisas do rato

--- lab4/mouse.c
+++ lab4/mouse.c
@@ -1,7 +1,9 @@
 #include "mouse.h"
-#include "queue.h"
 
-static queue_t byte_queue;
+static unsigned char packet[MOUSE_PACKET_SIZE];
+static unsigned char current_packet;
+
+static bool mouse_check_synchronization();
 
 int mouse_subscribe_int(unsigned* hook_id)
 {
@@ -12,6 +14,8 @@ int mouse_subscribe_int(unsigned* hook_id)
 	}
 	if (sys_irqsetpolicy(I8042_MOUSE_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, hook_id) == OK)
 	{
+		memset(packet, 0, sizeof(packet));	// Clean the array to make sure bit 3 is off in all bytes
+		current_packet = 0;
 		return hook_bit;
 	}
 	return -1;
@@ -27,7 +31,7 @@ int mouse_write(unsigned num_tries, unsigned char command)
 		{
 			return 1;
 		}
-		if (kbc_send_data(num_tries, I8042_WRITE_BYTE_TO_MOUSE))
+		if (kbc_send_data(num_tries, command))
 		{
 			return 1;
 		}
@@ -35,7 +39,7 @@ int mouse_write(unsigned num_tries, unsigned char command)
 		{
 			return 1;
 		}
-		if (response == I8042_MOUSE_RESPONSE_ACK)
+		if (response == MOUSE_RESPONSE_ACK)
 		{
 			return 0;
 		}
@@ -61,15 +65,52 @@ int mouse_send_argument(unsigned num_tries, unsigned char argument)
 		{
 			return 1;
 		}
-		if (response == I8042_MOUSE_RESPONSE_ACK)
+		if (response == MOUSE_RESPONSE_ACK)
 		{
 			return 0;
 		}
 	}
-	return 1;
+	return -1;
+}
+
+int mouse_int_handler(unsigned num_tries)
+{
+	unsigned char* output;
+	if ((output = malloc(sizeof(unsigned char))) == NULL)
+	{
+		return 1;
+	}
+	if (kbc_read(num_tries, output))
+	{
+		return 1;
+	}
+	// TODO sincronização
+	return 0;
 }
 
 int mouse_unsubscribe_int(unsigned hook_id)
 {
 	return kbc_unsubscribe_int(hook_id);
+}
+
+static bool mouse_check_synchronization()
+{
+	size_t i, j;
+	if (MOUSE_IS_POSSIBLE_FIRST_BYTE(packet[0]))	// Most of the times this will be true, so it's a good idea to check it first to avoid entering the loop
+	{
+		return true;
+	}
+	for (i = 1; i < MOUSE_PACKET_SIZE; ++i)
+	{
+		if (MOUSE_IS_POSSIBLE_FIRST_BYTE(packet[i]))
+		{
+			for (j = 0; j < MOUSE_PACKET_SIZE; ++j)
+			{
+				packet[j] = packet[(i + j) % MOUSE_PACKET_SIZE];
+				current_packet -= i;
+				return true;
+			}
+		}
+	}
+	return false;	// Couldn't find a byte with bit 3 set
 }
--- lab4/test4.c
+++ lab4/test4.c
@@ -1,5 +1,6 @@
 int test_packet(unsigned short cnt){
     /* To be completed ... */
+	// TODO para ler os pacotes é necessário verificar em que modo de operação se encontra o rato. Se estiver em stream mode, desativar, enviar os comandos e reativar, para evitar interferências de packets com respostas a comandos
 }	
 	
 int test_async(unsigned short idle_time) {
