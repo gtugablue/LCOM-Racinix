fc698e343712801c60282d47dd762aa6319af9e2
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-11-15 15:50:18 +0000

    SVN fix

--- lab5/lab5.d
+++
@@ -1,7 +0,0 @@
-
-lab5.o:	/usr/include/sys/null.h
-lab5.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/ansi.h
-lab5.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/stdlib.h
-lab5.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include/stdbool.h
-lab5.o:	lab5.c
-lab5.o:	video_gr.h
