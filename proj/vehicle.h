ce859cfb6e65916f4e7b433585b45f86c77737b4
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-11-15 21:02:45 +0000

    Vehicle physics

--- proj/vehicle.h
+++ proj/vehicle.h
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 #include "keyboard.h"
 #include "math.h"
+#include "video_gr.h"
 
-#define VEHICLE_ACCELERATE	50.0
-#define VEHICLE_DRAG		50.0
+#define VEHICLE_ACCELERATE	150.0
+#define VEHICLE_DRAG		0.5
 #define VEHICLE_BREAK		200.0
-#define VEHICLE_STEER_ANGLE	0.05
+#define VEHICLE_STEER_ANGLE	25.0
+#define VEHICLE_STOP_SPEED	10.0
 
 typedef struct
 {
@@ -21,7 +23,7 @@ typedef struct
 	double heading;
 } vehicle_t;
 
-vehicle_t *vehicle_create(double width, double length, const vector2D_t *position, double speed, double heading);
+vehicle_t *vehicle_create(double width, double length, const vector2D_t *position, double heading);
 
 void vehicle_tick(vehicle_t *vehicle, double delta_time, double max_velocity);
 
