77f2b02dfce3ebbee0143f9818c777b438ff3dc3
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-11-17 10:13:51 +0000

    lab5 comecado

--- lab5/test5.c
+++ lab5/test5.c
@@ -1,34 +1,35 @@
-void *test_init(unsigned short mode, unsigned short delay) {
-	
+#include "test5.h"
+
+void *test_init(unsigned short mode, unsigned short delay)
+{
+	char *video_mem = vg_init(mode);
+	sleep(delay);
 	/* To be completed */
-	
 }
 
 
-int test_square(unsigned short x, unsigned short y, unsigned short size, unsigned long color) {
-	
+int test_square(unsigned short x, unsigned short y, unsigned short size, unsigned long color)
+{
 	/* To be completed */
-	
 }
 
-int test_line(unsigned short xi, unsigned short yi, 
-		           unsigned short xf, unsigned short yf, unsigned long color) {
-	
+int test_line(unsigned short xi, unsigned short yi, unsigned short xf, unsigned short yf, unsigned long color)
+{
 	/* To be completed */
-	
 }
 
-int test_xpm(unsigned short xi, unsigned short yi, char *xpm[]) {
-	
+int test_xpm(unsigned short xi, unsigned short yi, char *xpm[])
+{
 	/* To be completed */
-	
 }	
 
-int test_move(unsigned short xi, unsigned short yi, char *xpm[], 
-				unsigned short hor, short delta, unsigned short time) {
-	
+int test_move(unsigned short xi, unsigned short yi, char *xpm[], unsigned short hor, short delta, unsigned short time)
+{
 	/* To be completed */
 	
 }					
 
-
+int test_controller()
+{
+	/* To be completed */
+}
