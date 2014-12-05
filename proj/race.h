4476dd18bd3a36dfc0dd81c76ef6956f42d97aae
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-12-05 22:36:26 +0000

    Commit de backup

--- proj/race.h
+++ proj/race.h
@@ -7,13 +7,13 @@
 typedef struct
 {
 	track_t *track;
-	unsigned num_vehicles;
+	unsigned num_players;
 	vehicle_t **vehicles;
 	double time; // A negative value means the race is in freeze time and about to start.
 	unsigned num_laps;
 } race_t;
 
-race_t *race_create(track_t *track, vehicle_t *vehicles[], unsigned num_vehicles, double freeze_time, unsigned num_laps);
+race_t *race_create(track_t *track, unsigned num_players, vehicle_t *vehicles[], double freeze_time, unsigned num_laps);
 
 int race_start(race_t *race);
 
