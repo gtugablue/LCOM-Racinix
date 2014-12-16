f5cc763c2cdfce6880046fecba77c094354a2d17
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-12-16 17:26:50 +0000

    Test code

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -5,10 +5,10 @@
 int main(int argc, char **argv) {
 
 	sef_startup();
-	/*unsigned hook_id = 5;
+	unsigned hook_id = 5;
 	serial_subscribe_int(&hook_id, 1);
 
-	unsigned char *string;*/
+	unsigned char *string;
 	/*while (1)
 	{
 		if (serial_receive_string(1, &string) == 0)
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
 		}
 	}*/
 
-	/*serial_set(1, 8, 1, 0, 9600);
+	serial_set(1, 8, 1, 0, 9600);
 
 	if (serial_fifo_receive_string(1, &string) == 0)
 	{
@@ -28,24 +28,7 @@ int main(int argc, char **argv) {
 		free(string);
 	}
 	printf("Unsubscribing...\n");
-	serial_unsubscribe_int(hook_id, 1);*/
-
-	queue_t *queue = queue_create();
-	size_t i;
-	for (i = 0; i < 10; ++i)
-	{
-		unsigned char *c = malloc(1);
-		*c = rand() % 255;
-		printf("Pushing char #%d...\n", *c);
-		queue_push(queue, c);
-	}
-queue_print(queue);
-	for (i = 0; i < 10; ++i)
-	{
-		unsigned char *c = queue_pop(queue);
-		printf("Popped char #%d.\n", *c);
-		free(c);
-	}
+	serial_unsubscribe_int(hook_id, 1);
 
 	return 0;
 }
