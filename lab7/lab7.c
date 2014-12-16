7ac3c1703415a245e08e6dab1b16764aa1db04a9
up201304143 <gtugablue@users.noreply.github.com>
2014-12-16 17:26:22 +0000

    Bug fix na queue

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -1,13 +1,14 @@
 #include "serial.h"
 #include "minix/driver.h"
+#include "queue.h"
 
 int main(int argc, char **argv) {
 
 	sef_startup();
-	unsigned hook_id = 5;
+	/*unsigned hook_id = 5;
 	serial_subscribe_int(&hook_id, 1);
 
-	unsigned char *string;
+	unsigned char *string;*/
 	/*while (1)
 	{
 		if (serial_receive_string(1, &string) == 0)
@@ -18,7 +19,7 @@ int main(int argc, char **argv) {
 		}
 	}*/
 
-	serial_set(1, 8, 1, 0, 9600);
+	/*serial_set(1, 8, 1, 0, 9600);
 
 	if (serial_fifo_receive_string(1, &string) == 0)
 	{
@@ -27,7 +28,24 @@ int main(int argc, char **argv) {
 		free(string);
 	}
 	printf("Unsubscribing...\n");
-	serial_unsubscribe_int(hook_id, 1);
+	serial_unsubscribe_int(hook_id, 1);*/
+
+	queue_t *queue = queue_create();
+	size_t i;
+	for (i = 0; i < 10; ++i)
+	{
+		unsigned char *c = malloc(1);
+		*c = rand() % 255;
+		printf("Pushing char #%d...\n", *c);
+		queue_push(queue, c);
+	}
+queue_print(queue);
+	for (i = 0; i < 10; ++i)
+	{
+		unsigned char *c = queue_pop(queue);
+		printf("Popped char #%d.\n", *c);
+		free(c);
+	}
 
 	return 0;
 }
