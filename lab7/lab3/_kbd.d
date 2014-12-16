5c79f6efb8287fc674aee04bd7ef00a3a59fc95e
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-12-16 00:43:40 +0000

    ...

--- lab7/lab3/_kbd.d
+++
@@ -1,4 +0,0 @@
-
-_kbd.o:	_kbd.S
-_kbd.o:	_kbd.h
-_kbd.o:	i8042.h
--- lab7/lab5/xpm.h
+++
@@ -1,6 +0,0 @@
-#ifndef _XPM_H
-#define _XPM_H
-
-char *read_xpm(char *map[], int *wd, int *ht, unsigned short h_res, unsigned short v_res);
-
-#endif
