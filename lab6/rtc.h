671738e6ecb5596b1843b9d8178ce4eca98fa394
up201305973 <EdgarDuarte14@users.noreply.github.com>
2014-12-28 17:55:30 +0000

    constantes

--- lab6/rtc.h
+++ lab6/rtc.h
@@ -9,6 +9,17 @@
 #define RTC_ADDR_REG 0x70
 #define RTC_DATA_REG 0x71
 
+#define RTC_ADDRESS_SECONDS 		0
+#define RTC_ADDRESS_SECONDS_ALARM 	1
+#define RTC_ADDRESS_MINUTES			2
+#define RTC_ADDRESS_MINUTES_ALARM	3
+#define RTC_ADDRESS_HOURS 			4
+#define RTC_ADDRESS_HOURS_ALARM 	5
+#define RTC_ADDRESS_DAY_WEEK 		6
+#define RTC_ADDRESS_DAY_MONTH		7
+#define RTC_ADDRESS_MONTH			8
+#define RTC_ADDRESS_YEAR			9
+
 #define RTC_IRQ_LINE 8 //irq_line
 
 int rtc_subscribe_int(unsigned *hook_id);
