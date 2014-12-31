7d6864c6637fdab474004ba2ba8d6a6fc132d781
up201304143 <up201304143@b247c712-c40c-45e8-8384-0ff1c61b37f2>
2014-12-31 00:43:13 +0000

    Added copyright notice to isPointInPolygon() and changed the name of a variable in the vehicle collision calculations

--- proj/code/vector2D.c
+++ proj/code/vector2D.c
@@ -94,6 +94,18 @@ bool isPointInAxisAlignedRectangle(vector2D_t top_left_corner, unsigned width, u
 
 bool isPointInPolygon(vector2D_t polygon[], unsigned polygon_size, vector2D_t point)
 {
+	/*
+	Copyright (c) 1970-2003, Wm. Randolph Franklin
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+		1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimers.
+		2. Redistributions in binary form must reproduce the above copyright notice in the documentation and/or other materials provided with the distribution.
+		3. The name of W. Randolph Franklin may not be used to endorse or promote products derived from this Software without specific prior written permission.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	*/
+
 	int i, j;
 	bool c = false;
 	for (i = 0, j = polygon_size - 1; i < polygon_size; j = i++) {
